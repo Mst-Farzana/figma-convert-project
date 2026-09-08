4c64f58461be6e387ea665d2dc6be17ca4cbc225
Farzana <farzanap1531994@gmail.com>
2026-09-08 23:25:03 +0600

    Updates

--- _next/static/EEZK7-zSKNNf-QJlwSA7g/_buildManifest.js
+++ _next/static/EEZK7-zSKNNf-QJlwSA7g/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/figma-convert-project//_next/:path+",
+        "destination": "/figma-convert-project/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
