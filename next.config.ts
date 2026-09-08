e6211fa64e88ec83bb631802e9de932366babbae
Farzana <farzanap1531994@gmail.com>
2026-09-08 23:40:49 +0600

    fix: add trailingSlash for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-
   basePath: '/figma-convert-project',
   assetPrefix: '/figma-convert-project/',
-
   images: {
     unoptimized: true,
   },
