20a07f802446f57218cebd44a08f95e53a43a71c
Farzana <farzanap1531994@gmail.com>
2026-09-09 00:25:41 +0600

    chore: clean next config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/figma-convert-project',
-  assetPrefix: '/figma-convert-project/',
   images: {
     unoptimized: true,
   },
